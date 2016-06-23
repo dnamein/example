a11872cfcd05c546295344d684423985cbfe7b76
dnamein <dnamein@gmail.com>
2016-06-23 10:23:35 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -26,51 +26,16 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-       		document.addEventListener("deviceready", function () {
-				if (window.cordova) {
-						alert('window.cordova is available');
-					} else {
-						alert('window.cordova NOT available');
-					}
-
-				if (window.cordova && window.cordova.plugins) {
-					alert('window.cordova.plugins is available');
-				} else {
-					alert(window.cordova.plugins);
-				}
-				window.plugins.BarcodeScanner.scan(
-				  function (result) {
-					  alert("We got a barcode\n" +
-							"Result: " + result.text + "\n" +
-							"Format: " + result.format + "\n" +
-							"Cancelled: " + result.cancelled);
-				  }, 
-				  function (error) {
-					  alert("Scanning failed: " + error);
-				  },
-				  {
-					  "preferFrontCamera" : true, // iOS and Android
-					  "showFlipCameraButton" : true, // iOS and Android
-					  "prompt" : "Place a barcode inside the scan area", // supported on Android only
-					  "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-					  "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
-				  }
-			   );
-			}, false);
-			
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+		 document.getElementById('scan').addEventListener('click', this.scan, false);
+        document.getElementById('encode').addEventListener('click', this.encode, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		
-
-
-		
-		
-		
-		
+		console.log('onDeviceReady');
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
@@ -83,10 +48,64 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
-};
-function skanuj() {
-	console.log(plugins.barcodeScanner);
+    },
+	 
 
+    encode: function() {
+		
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.encode(scanner.Encode.TEXT_TYPE, "http://www.nhl.com", function(success) {
+            alert("encode success: " + success);
+          }, function(fail) {
+            alert("encoding failed: " + fail);
+          }
+        );
+
+    }
+	
+};
+function scan()
+{
+	alert("scan");
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if(!result.cancelled)
+            {
+                if(result.format == "QR_CODE")
+                {
+                    navigator.notification.prompt("Please enter name of data",  function(input){
+                        var name = input.input1;
+                        var value = result.text;
+
+                        var data = localStorage.getItem("LocalData");
+                        console.log(data);
+                        data = JSON.parse(data);
+                        data[data.length] = [name, value];
+
+                        localStorage.setItem("LocalData", JSON.stringify(data));
+
+                        alert("Done");
+                    });
+                }
+            }
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+   );
 }
-app.initialize();
+function takephoto()
+{
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+destinationType: Camera.DestinationType.DATA_URL
+});
+}
+function onSuccess(imageData) {
+var image = document.getElementById('myImage');
+image.src = "data:image/jpeg;base64," + imageData;
+document.getElementById("text1").innerHTML = imageData;
+}
+function onFail(message) {
+alert('Failed because: ' + message);
+}
